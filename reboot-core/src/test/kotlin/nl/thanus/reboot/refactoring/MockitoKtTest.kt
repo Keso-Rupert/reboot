dd5c4e8d288457e760fcb83c873e05df064a1932
thanus <thanus@users.noreply.github.com>
2020-03-11 09:27:17 +0100

    Resolve constructor arguments

--- reboot-core/src/test/kotlin/nl/thanus/reboot/refactoring/MockitoKtTest.kt
+++ reboot-core/src/test/kotlin/nl/thanus/reboot/refactoring/MockitoKtTest.kt
@@ -1,6 +1,10 @@
 package nl.thanus.reboot.refactoring
 
 import com.github.javaparser.StaticJavaParser
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration
+import com.github.javaparser.symbolsolver.JavaSymbolSolver
+import com.github.javaparser.symbolsolver.resolution.typesolvers.MemoryTypeSolver
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
 
 internal class MockitoKtTest : ReBootBase() {
@@ -65,6 +69,41 @@ internal class MockitoKtTest : ReBootBase() {
                 private UsersController usersController;
             }
         """.trimIndent()
+
+        val typeDeclaration = mockk<ResolvedReferenceTypeDeclaration>()
+
+        val memoryTypeSolver = MemoryTypeSolver()
+        memoryTypeSolver.addDeclaration("example.controllers.UsersController", typeDeclaration)
+
+        StaticJavaParser.getConfiguration().setSymbolResolver(JavaSymbolSolver(memoryTypeSolver))
+
+        val compilationUnit = StaticJavaParser.parse(code)
+
+        rewriteMockitoFieldInjections(compilationUnit)
+
+        val expectedCode = """
+            class UsersControllerTest {
+                private UsersController usersController = new UsersController();
+            }
+        """.trimIndent()
+
+        assertRefactored(compilationUnit, expectedCode)
+    }
+
+    @Test
+    fun `Rewrite @InjectMocks with object creation cannot resolve`() {
+        val code = """
+            import org.mockito.InjectMocks;
+            
+            class UsersControllerTest {
+                @InjectMocks
+                private UsersController usersController;
+            }
+        """.trimIndent()
+
+        val memoryTypeSolver = MemoryTypeSolver()
+        StaticJavaParser.getConfiguration().setSymbolResolver(JavaSymbolSolver(memoryTypeSolver))
+
         val compilationUnit = StaticJavaParser.parse(code)
 
         rewriteMockitoFieldInjections(compilationUnit)
